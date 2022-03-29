a3e68fecf1cf6d5f1a1fc910c63f13d9f4e68982
Adam Turnbull <adam.t.turnbull@gmail.com>
2022-03-29 12:54:42 -0700

    babel

---
+++ dist/components/TestDiv.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var TestDiv = function TestDiv(props) {
+  return /*#__PURE__*/_react["default"].createElement("div", null, "I am a private repository!");
+};
+
+var _default = TestDiv;
+exports["default"] = _default;
---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "TestDiv", {
+  enumerable: true,
+  get: function get() {
+    return _TestDiv["default"];
+  }
+});
+
+var _TestDiv = _interopRequireDefault(require("./components/TestDiv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
